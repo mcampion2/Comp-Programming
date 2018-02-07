af040ffaaf870751a321adee7bb8a3ec4823e921
Multimedia Intro <mmintro@nvc.ad.root>
2018-02-07 12:02:03 -0600

    Initial commit

---
+++ rubrics2.js
@@ -0,0 +1,35 @@
+// JavaScript Documentfunction addNumbers (a,b){
+
+		function addNumbers (d,e){
+		'use strict';
+		var addedTotal= d + e;
+		addedTotal.toString();
+		alert (addedTotal);
+	}
+	
+	addNumbers(9,10);
+	
+	var studentName= prompt ("What is the students name?");
+	
+	switch (studentName){
+		case "Michael":
+		alert ("Awesome");
+		break;
+			
+		case "Fransisco":
+		alert ("Super Awesome");
+		break;
+			
+		default:
+		alert ("I dont think that student exsist!");	
+					   }
+
+		function addNumbers (a,b){
+		'use strict';
+		var addedTotal= a + b;
+		return addedTotal;
+	}
+	
+		var addNumbers2= addNumbers(9,10);
+		alert (addNumbers2);
+	
